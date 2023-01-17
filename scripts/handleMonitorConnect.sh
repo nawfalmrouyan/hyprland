9d370c42ef13f49eb3c4314bc438f073096cc52f
Nawfal <nawfalmrouyan@gmail.com>
2023-01-17 18:44:32 +0800

    fix: add $HYPRLAND_INSTANCE_SIGNATURE to socket path

--- scripts/handleMonitorConnect.sh
+++ scripts/handleMonitorConnect.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function handle {
   if [[ ${1:0:12} == "monitoradded" ]]; then
@@ -10,4 +10,4 @@ function handle {
   fi
 }
 
-socat - UNIX-CONNECT:/tmp/hypr/.socket2.sock | while read line; do handle $line; done
+socat - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read line; do handle $line; done
