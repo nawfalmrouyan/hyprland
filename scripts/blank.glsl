dc436dc616ec01b89cc48c8df7c92aed74ea2d55
Nawfal <nawfalmrouyan@gmail.com>
2023-08-19 00:42:52 +0800

    feat: use bluefilter shader

---
+++ scripts/blank.glsl
@@ -0,0 +1,10 @@
+// blank shader
+
+precision mediump float;
+varying vec2 v_texcoord;
+uniform sampler2D tex;
+
+void main() {
+    vec4 pixColor = texture2D(tex, v_texcoord);
+    gl_FragColor = pixColor;
+}
