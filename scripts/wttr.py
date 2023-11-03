0a75d0d1b62d658fd5a22e16f3a37b5ffde16fa7
Nawfal <nawfalmrouyan@gmail.com>
2023-11-03 09:46:33 +0800

    fixes: more icons size, alignment fixes.

--- scripts/wttr.py
+++ scripts/wttr.py
@@ -94,7 +94,7 @@ if tempint > 0 and tempint < 10:
     extrachar = '+'
 
 
-data['text'] = '<span font="11" rise="1500">'+WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
+data['text'] = '<span font="10" rise="1500">'+WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
     "</span>"+extrachar+weather['current_condition'][0]['FeelsLikeC']+"°"
 
 data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
