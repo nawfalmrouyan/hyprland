dc71784219a1389332db44c6fc15805e686f3ff7
Nawfal <nawfalmrouyan@gmail.com>
2024-04-25 14:09:11 +0800

    make changing wallpaper also changes lockscreen bg

--- scripts/generateLockscreenWallpaper.sh
+++ scripts/generateLockscreenWallpaper.sh
@@ -4,12 +4,9 @@ CACHE=cache.png
 BLURRED=blurred.png
 SQUARE=square.png
 DIR=$HOME/.config/hypr/bg
-PICS=($(ls ${DIR}))
-
-RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
 
 #convert to PNG
-convert $DIR/$RANDOMPICS $DIR/$CACHE
+convert $1 $DIR/$CACHE
 
 # blur wallpaper
 magick $DIR/$CACHE -blur "50x30" $DIR/$BLURRED
