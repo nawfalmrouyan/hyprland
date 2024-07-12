c16276a85c8acc8513073642bb2ee661ec284f42
Nawfal <nawfalmrouyan@gmail.com>
2024-07-12 09:44:13 +0800

    no border

--- scripts/gamemode.sh
+++ scripts/gamemode.sh
@@ -7,7 +7,7 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
         keyword decoration:blur:enabled 0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
+        keyword general:border_size 0;\
         keyword decoration:rounding 0"
         notify-send -u low "Hyprland" "Game Mode ENABLED"
         exit
