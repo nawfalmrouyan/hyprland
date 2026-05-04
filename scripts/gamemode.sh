87900e357893402e4b3e359f1e65377e41e430e9
OpalBSD <nawfalmrouyan@gmail.com>
2026-05-05 07:42:46 +0800

    Update game mode script to use lua.

--- scripts/gamemode.sh
+++ scripts/gamemode.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env sh
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-if [ "$HYPRGAMEMODE" = 1 ]; then
-        hyprctl --batch "\
-        keyword animations:enabled 0;\
-        keyword decoration:drop_shadow 0;\
-        keyword decoration:blur:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 0;\
-        keyword decoration:rounding 0"
+
+if [ "$HYPRGAMEMODE" = "true" ]; then
+        hyprctl eval 'hl.config({ animations = { enabled = false }, decoration = { shadow = { enabled = false }, rounding = 0, blur = { enabled = false } }, general = { gaps_in = 0, gaps_out = 0, border_size = 0 } })'
         notify-send -u low "Hyprland" "Game Mode ENABLED"
 else
         hyprctl reload
